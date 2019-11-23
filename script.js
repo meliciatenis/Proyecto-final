c4950f5a47d3fb2115bddbf8752d1145ce217ba7
melicia <meliciatenis@gmail.com>
2019-11-23 13:55:45 -0300

    funcionalidad de los fav

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let resultados = {};
 let conteudo = "" ;
 
 buscador.addEventListener("keyup",(e) => {
-
     if (e.key == "Enter") {
         primero();
     }
@@ -26,12 +25,12 @@ function primero() {
 
 function search() {
 
-    let url = "https://api.giphy.com/v1/gifs/search?api_key=CoGMIvpPxYlGK6JvJDyJoqYksgDZ5kwG&limit=15&offset=" + add +"&q=" + encodeURI(conteudo);
+    let url = "https://api.giphy.com/v1/gifs/search?api_key=CoGMIvpPxYlGK6JvJDyJoqYksgDZ5kwG&limit=20&offset=" + add +"&q=" + encodeURI(conteudo);
 
     console.log(url);
     buscador.value = "";
 
-    add = add + 10;   
+    add = add + 20;   
 
     fetch(url).then((response) => response.json()).then((data) => {
         resultados = data.data
@@ -39,29 +38,45 @@ function search() {
     });
 }
 
+function trocar(atual){ 
+
+    if (document.getElementById(atual.srcElement.id).dataClickado == false) {
+        document.getElementById(atual.srcElement.id).dataClickado = true ;
+        document.getElementById(atual.srcElement.id).src = "cor1.png" ;
+    }
+    else {
+        document.getElementById(atual.srcElement.id).dataClickado = false ;
+        document.getElementById(atual.srcElement.id).src = "cor.png" ;
+    }
+
+}
+
 function respuesta() {
 
     const contenedor = document.querySelector("#resultados");
     let mas = document.querySelector("#more");
     if (mas !== null ) { contenedor.removeChild(mas) }
-    mas = document.createElement("button");
-    mas.textContent = "Carregar Mais";
+    mas = document.createElement("img");
+    mas.src = "arrow1.png"
     mas.id = "more";
 
     for (let item of resultados) {
         let img = document.createElement("img");
-        img.src = item.images.original.url;
+        img.src = item.images.fixed_width.url;
 
-        const fav = document.createElement("button")
+        let fav = document.createElement("img")
+        fav.src = "cor.png" 
+        fav.id = "cor_" + item.id; 
         
-
         contenedor.appendChild(img);
         contenedor.appendChild(mas);
         contenedor.appendChild(fav);
 
         mas.addEventListener("click", search);
+        fav.addEventListener("click", trocar);
 
     }
+    let favFinal = document.querySelector("#resultados img:last-child")
+    
+    contenedor.removeChild(favFinal)
 };
-
-
